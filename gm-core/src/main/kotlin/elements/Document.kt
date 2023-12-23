f608d6a05c0e2313459da30a854dbb9e2a4021db
modo_lv <modo@modo.lv>
2023-12-24 01:58:15 +0200

    Add: `Document` element.

---
+++ gm-core/src/main/kotlin/elements/Document.kt
@@ -0,0 +1,23 @@
+package elements
+
+import elements.types.BlockContainer
+
+/**
+ * The root element for a GM page, contains all other elements.
+ */
+class Document : Element(), BlockContainer {
+    /**
+     * Path to the file that this document come from, relative to the project's content dir.
+     */
+    lateinit var filePath: String
+
+    /**
+     * Unique document identifier, based on the file path.
+     */
+    val id: String get() = filePath.removeSuffix(".md")
+
+    /**
+     * How deep down this document is in the project file tree (relative to the project's content dir).
+     */
+    val fileDepth get() = filePath.count { it == '/' }
+}
---
+++ gm-core/src/test/kotlin/DocumentTests.kt
@@ -0,0 +1,18 @@
+import elements.Document
+import main.FromMarkdown
+import org.amshove.kluent.`should be equal to`
+import org.junit.jupiter.api.Test
+
+class DocumentTests {
+    @Test
+    fun `ID is set correctly based on its file path`() {
+        Document().apply { filePath = "chapter1/page1.md" }
+            .id `should be equal to` "chapter1/page1"
+    }
+
+    @Test
+    fun `Document depth is correctly based on its file path`() {
+        Document().apply { filePath = "part1/chapter1/page1.md" }
+            .fileDepth `should be equal to` 2
+    }
+}
