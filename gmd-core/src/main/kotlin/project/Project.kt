cd94a7f4e9c307aa38f5a38e2971b4ef14dfe555
modo_lv <modo@modo.lv>
2024-02-14 19:51:55 +0200

    Add: Basic config loading.

--- gmd-core/src/main/kotlin/project/Project.kt
+++ gmd-core/src/main/kotlin/project/Project.kt
@@ -1,7 +1,10 @@
 package project
 
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory
+import com.typesafe.config.ConfigParseOptions
+import com.typesafe.config.ConfigSyntax
 import java.nio.file.Path
-import kotlin.io.path.name
 
 /**
  * Base class for GMD projects.
@@ -12,12 +15,34 @@ open class Project(rootPath: Path) {
     /**
      * project.Project name, for display and logging purposes.
      */
-    var name: String = rootPath.name
+    val name: String by lazy { settings.name }
 
     /**
      * Path to source Markdown pages within the project.
      */
     var pageDir: Path = Path.of("pages")
+
+    private var _settings: ProjectSettings? = null
+    var settings: ProjectSettings
+        get() = _settings ?: replaceSettings(this.rootPath.resolve("gmd.conf"))
+        protected set(value) {
+            _settings = value
+        }
+
+    fun replaceSettings(vararg configFiles: Path): ProjectSettings {
+        val newSettings = ProjectSettings()
+        configFiles.forEach { configPath ->
+            val conf = ConfigFactory.parseFile(
+                configPath.toFile(),
+                ConfigParseOptions.defaults().setAllowMissing(true).setSyntax(ConfigSyntax.CONF)
+            ).ifConfig("core") { it } ?: ConfigFactory.empty()
+
+            conf.ifString("name") { newSettings.name = it }
+        }
+        if (newSettings.name.isBlank())
+            throw Exception("Project must have a name configured.")
+        return newSettings
+    }
 
 
     override fun toString(): String =
@@ -26,4 +51,10 @@ open class Project(rootPath: Path) {
                 "${this::name.name}=${this.name}, " +
                 "${this::pageDir.name}=${this.pageDir}" +
                 ")"
+
+    fun <T> Config.ifString(path: String, f: (String) -> T): T? =
+        if (this.hasPath(path)) f(this.getString(path)) else null
+
+    fun <T> Config.ifConfig(path: String, f: (Config) -> T): T? =
+        if (this.hasPath(path)) f(this.getConfig(path)) else null
 }
---
+++ gmd-core/src/main/kotlin/project/ProjectSettings.kt
@@ -0,0 +1,8 @@
+package project
+
+/**
+ * Project settings as loaded from config file(s).
+ */
+open class ProjectSettings {
+    var name: String = ""
+}
