4b0e9e4fb96f6d0f88c100c2ff991fd1795de61d
modo_lv <modo@modo.lv>
2024-02-14 14:19:15 +0200

    Rename: `WorkContext` to `BuildContext`.

---
+++ gmd-core/src/main/kotlin/build/BuildContext.kt
@@ -0,0 +1,6 @@
+package build
+
+/**
+ * Stores and provides access to contextual data for workers during MD->GMD conversion process.
+ */
+class BuildContext
--- gmd-core/src/main/kotlin/build/WorkContext.kt
+++
@@ -1,7 +0,0 @@
-package build
-
-/**
- * Stores and provides access to contextual data for workers in a given build.
- */
-class WorkContext {
-}
