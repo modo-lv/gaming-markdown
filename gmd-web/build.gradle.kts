fa059ddd914bbca03047199b278fd15b442e99e1
modo_lv <modo@modo.lv>
2024-08-25 00:50:01 +0300

    [gmd-web] Add JLessC

--- gmd-web/build.gradle.kts
+++ gmd-web/build.gradle.kts
@@ -6,6 +6,7 @@ dependencies {
     implementation(libs.cli.args)
     implementation(libs.kte)
     implementation(project(":gmd-core"))
+    implementation("de.inetsoftware:jlessc:1.12")
 
     testImplementation(libs.bundles.core.test)
 }
